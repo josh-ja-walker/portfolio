7f4f2f3c3bb71a07aa112bc1a5b81bb8b0ad7b62
Josh Walker <josh.walker1@outlook.com>
2024-09-12 00:26:20 +0100

    Repunctuated about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ function About() {
         <div id='About' className='Section'>
             <header>About</header>
             <p>
-                Third-year student at Imperial College London, studying MEng Computing; I have 
+                Third-year student at Imperial College London, studying MEng Computing. I have 
                 a strong interest in games and software development, explored through 
                 game jams and university projects which were instrumental in building
                 foundational teamworking skills.
