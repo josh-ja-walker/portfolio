d5b2b554973167a6cb54d5afd290b416acfe42f7
Josh Walker <j.walker1005@outlook.com>
2026-05-02 16:14:59 +0100

    feat: update about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,7 @@ function About() {
         <div className='about section'>
             <header>About</header>
             <p>
-                Third-year student at Imperial College London, studying MEng Computing. I have 
-                a strong interest in games and software development, explored through 
-                game jams and university projects which were instrumental in building
-                foundational teamworking skills.
+                Fourth-year student at Imperial College London, studying MEng Computing, with a strong interest in graphics programming and its application in games and software development. 
             </p>
         </div>
     );
